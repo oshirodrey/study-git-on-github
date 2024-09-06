562231a7a71bbf1e897d993d9a0ffe7b3cb34cd0
OshiroDrey <is this saved?>
2024-09-06 10:49:26 +0700

    hmm how tf

--- try.py
+++ try.py
@@ -1,6 +1,8 @@
+import time
 print("hello world")
 print("i just wanna say")
-"""I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
+"""str(input("waiting to exit "))"""
+print("""I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
@@ -63,5 +65,5 @@ I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
 I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
-I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust 
-"""
+I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust I love Faust """)
+time.sleep(1)
